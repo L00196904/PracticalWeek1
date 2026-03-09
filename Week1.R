267b42c3c371f2381a9a5425311ba1c76b649fad
sakun9526 <sakunrathnayaka@yahoo.com>
2026-03-09 12:06:00 +0000

    practical week 1

---
+++ Week1.R
@@ -0,0 +1,29 @@
+x <- c(3,12,7)
+y <- c(4,9,11)
+
+z <- x + y
+z
+
+(x+2)
+(y+50)
+
+(sum(x))
+
+(mean(y))
+
+w <- c(2,7,NA,20)
+sum(w)
+
+v <- sum(w, na.rm = TRUE)
+v
+
+typeof(v)
+
+x <- append(x, 12)
+x
+
+u <- c(5:20)
+u
+
+u <- append(5:20, letters[6:10])
+u
